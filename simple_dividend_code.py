803180413839249d48dcdc4349e386455916f4d9
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2019-05-09 10:51:43 -0700

    Add files via upload

--- simple_dividend_code.py
+++ simple_dividend_code.py
@@ -8,6 +8,8 @@ and a number of years to print out a persons ROI.
 Author:Tahwab Noori
 Date:May 9, 2019
 """
+import os
+
 
 initial = float(input("Initial investment?"))
 dividend = float(input("Yearly dividend yield?"))
@@ -20,3 +22,4 @@ for iter in range(1, years + 1):
    initial += monthly * 12
    #.format is used to give the initial 2 decimal places
    print ("Year %d:{0:.2f}".format(initial) % iter)
+os.system("pause")
