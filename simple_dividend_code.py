1a0e071cb7e3c299ee3c71f2df38936fa736009b
Tahwab Noori <36939802+taco-coder@users.noreply.github.com>
2019-05-14 11:35:32 -0700

    Add files via upload
    Created new function to allow user to overwrite specific files. Refracted code. Added more error handling and invalid input responses.

--- simple_dividend_code.py
+++ simple_dividend_code.py
@@ -14,6 +14,7 @@ initial = float(input("Initial investment?"))
 dividend = float(input("Yearly dividend yield?"))
 monthly = float(input("Monthly contribution?"))
 years = int(input("Number of years?"))
+currentTime = time.strftime("%Y_%m_%d@%Hh%Mm%Ss")
 
 #noReinvest takes the years, monthly, initial, and dividend float values and 
 #calculates the users yearly returns at that fixed rate
@@ -36,7 +37,7 @@ def reinvest(years, monthly, initial, dividend):
      interval = int(input("How many times a year would you like to interval your monthly contributions?"))
      reinvest = float(input("How much would you like to interval your contribution by each time?"))      
      value = str(input("Would you like to print out monthly returns as well?(y/n)"))
-     output = fileMaker()
+     outfile = fileMaker()
      #for loop with nested loop to 
      for iter in range(1, years + 1): #how many years of investing
             #loop finds the return each month
@@ -50,21 +51,25 @@ def reinvest(years, monthly, initial, dividend):
                     #.format is used to give the initial 2 decimal places                
                     if(value == 'y' or 'Y'):
                         print("End of month %d:${0:.2f}".format(initial) % (j + (i * (12 / interval))))
-                        output.write("End of month %d:${0:.2f}".format(initial) % (j + (i * (12 / interval))))
+                        outfile.write("End of month %d:${0:.2f}".format(initial) % (j + (i * (12 / interval))))
                 #reinvestment amount increases
                 monthly += reinvest
                 
                 print('Investment interval: ' + str(reinvest))
-                output.write('Investment interval: ' + str(reinvest))
+                outfile.write('Investment interval: ' + str(reinvest))
                 print('Current reinvestment amount: ' + str(monthly))
-                output.write('Current reinvestment amount: ' + str(monthly))
+                outfile.write('Current reinvestment amount: ' + str(monthly))
                 
             print("End of year %d:${0:.2f}".format(initial) % iter)
-            output.write("End of year %d:${0:.2f}".format(initial) % iter)
-     output.close()
-
+            outfile.write("End of year %d:${0:.2f}".format(initial) % iter)
+     outfile.close()
+#fileMaker asks the user if they would like to save the data to a textfile.
+#it checks if the the appropriate directory exists and creates it if necessary. It gives the user the option
+#of overwriting an existing save file or creating a new file, respectivly. Calls the fileOverWriter function for overwriting.
 def fileMaker():
+    
     val = str(input("Would you like to save your data to a textfile?(y/n)"))
+    
     if(val == 'y' or 'Y'):
         currentPath = os.getcwd()
         #checks if directory currently exists and creates directory if it doesn't.
@@ -74,18 +79,39 @@ def fileMaker():
             outputPath = currentPath + '/output_files'
             os.chdir(outputPath) 
         if len(os.listdir(outputPath)) == '0':
-            currentTime = time.strftime("%Y_%m_%d@%Hh%Mm%Ss")
             output = open("output-%s.txt" % currentTime, 'w')
         else:
-            value = str(input("There currently exists a previous savefile. Would you like to overwrite or create a new save file?\n(O = overwrite/C = create new file)"))
-            if (value == 'c' or 'C'):
-                currentTime = time.strftime("%Y_%m_%d@%Hh%Mm%Ss")
-                output = open("output-%s.txt" % currentTime, 'w')
-            elif(value == 'o' or 'O'):
-                 for iter in os.listdir(outputPath):
-                     print('%d-' % iter)
+            control = False
+            while(control == False):
+                option = str(input("There currently exists some previous savefile(s). Would you like to overwrite or create a new save file?\n(O = overwrite/C = create new file)"))
+                if (option == "c"):
+                    control = True
+                    output = open("output-%s.txt" % currentTime, 'w')
+                elif(option == "o"):
+                    control = True
+                    output = open(fileOverWriter(outputPath), 'w')
+                else:
+                    print("Invalid input. Please try again.")
         return output
-
+#fileOverWriter takes the outputPath of the folder containing the output files and prints them to the user.
+#The user then chooses a file from the list to overwrite. The file is respectively renamed and the newly named file is returned.
+def fileOverWriter(outputPath):
+    outputList = os.listdir(outputPath)
+    
+    for iter in range(len(outputList)):
+        print('%d-' % iter + outputList[iter] + '\n')
+    indexValue = False
+    while indexValue == False:
+        numfile = int(input("Please pick which number file.(eg 1, 2, 3, etc.)"))
+        try:
+            indexValue = True
+            overwriteFile = outputList[numfile]
+            newName = "output-%s.txt" % currentTime
+            os.rename(overwriteFile, newName)
+        except IndexError:
+            print("Invalid input. That file index does not exist. Please try again.")
+    return newName
+        
 #dynamic sets the values for all the args and params then calls the appropriate functions based on the users input
 def dynamic():
     value = str(input("Would you like the option of increasing your monthly contributions after some months? (y/n)"))
